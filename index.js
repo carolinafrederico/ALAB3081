578949711ecb09151e70c1b0138c4456619a4a13
carolinafrederico <iam@carolinafrederico.com>
2025-02-26 22:49:53 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+let result1 = 10/5
+console.log(result1)
+let result2 = 15/5
+console.log(result2)
+let result3 = 20/5
+console.log(result3)
+let result4 = 5/5
+console.log(result4)
