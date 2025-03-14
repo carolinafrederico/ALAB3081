527bdf9810ac60dc6c316d014877b96562e150ac
carolinafrederico <iam@carolinafrederico.com>
2025-03-14 13:16:00 -0400

    Another attempt

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const n1 = 10;
 const n2 = 15;
 const n3 = 20;
 const n4 = 5;
-let result1 = 10/5
+let result1 = n1/5
 console.log(result1)
-let result2 = 15/5
+let result2 = n2/5
 console.log(result2)
-let result3 = 20/5
+let result3 = n3/5
 console.log(result3)
-let result4 = 5/5
+let result4 = n4/5
 console.log(result4)
 console.log(n1 > n4);
 console.log(n2-n1)
